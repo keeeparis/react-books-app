2137bef17da58f0e6d2a101fb4a1601476466091
Keeeparis <vladimir1trotsenko@gmail.com>
2022-04-21 17:52:57 +0600

    added store

---
+++ src/redux/store/store.ts
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/dist/query'
+import mainReducer from '../mainFeature/mainSlice'
+import { booksApi } from '../services/books'
+
+export const store = configureStore({
+  reducer: {
+    main: mainReducer,
+    [booksApi.reducerPath]: booksApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(booksApi.middleware),
+})
+
+// setupListeners(store.dispatch)
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
