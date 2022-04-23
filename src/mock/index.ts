fb99be56f6f5d001e35db25e53fddee45b14ec64
Keeeparis <vladimir1trotsenko@gmail.com>
2022-04-23 15:39:01 +0600

    add mock

---
+++ src/mock/index.ts
@@ -0,0 +1,12 @@
+export const categories = [
+  'All',
+  'Art',
+  'Biography',
+  'Computers',
+  'History',
+  'Medical',
+  'Poetry',
+  'Fiction',
+]
+
+export const sorting = ['relevance', 'newest']
